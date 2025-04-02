fdb9e07f553044bf401f73263ddb993914bf0913
Carlos <dev_carlos@outlook.com.br>
2025-04-01 23:18:33 -0300

    nix

--- flake.nix
+++ flake.nix
@@ -1,52 +1,64 @@
 {
-  description = "Homelab";
+  description = "Homelab Development Environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        ansible
-        ansible-lint
-        bmake
-        diffutils
-        docker
-        docker-compose
-        dyff
-        git
-        go
-        gotestsum
-        iproute2
-        jq
-        k9s
-        kanidm
-        kube3d
-        kubectl
-        kubernetes-helm
-        kustomize
-        libisoburn
-        neovim
-        openssh
-        opentofu
-        p7zip
-        pre-commit
-        qrencode
-        shellcheck
-        wireguard-tools
-        yamllint
-        (python3.withPackages (ps: with ps; [
-          jinja2
-          kubernetes
-          mkdocs-material
-          netaddr
-          pexpect
-          rich
-        ]))
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          # Lista de pacotes necessários para o ambiente de desenvolvimento
+          packages = with pkgs; [
+            ansible
+            ansible-lint
+            bmake
+            diffutils
+            docker
+            docker-compose
+            dyff
+            git
+            go
+            gotestsum
+            iproute2
+            jq
+            k9s
+            kanidm
+            kube3d
+            kubectl
+            kubernetes-helm
+            kustomize
+            libisoburn
+            neovim
+            openssh
+            opentofu
+            p7zip
+            pre-commit
+            qrencode
+            shellcheck
+            wireguard-tools
+            yamllint
+            (python3.withPackages (ps: with ps; [
+              jinja2
+              kubernetes
+              mkdocs-material
+              netaddr
+              pexpect
+              rich
+            ]))
+          ];
+
+          # Comandos executados ao entrar no ambiente de desenvolvimento
+          shellHook = ''
+            echo "Bem-vindo ao ambiente de desenvolvimento do Homelab!"
+            # Adicione aqui quaisquer comandos adicionais que deseja executar ao iniciar o shell
+          '';
+        };
+      }
+    );
 }
